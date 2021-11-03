9f9347a77c36d95cc3c72b5fda681e035cff77fb
Jordan Johnston <1086jjohnston@gmail.com>
2021-11-03 12:09:15 -0500

    updated JavaScript

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,1 +1,30 @@
-window.alert("This is an alert! JavaScript is running!");
+var playerName = window.prompt("What is your robot's name?");
+var playerHealth = 100;
+var playerAttack = 10;
+
+//You can also log multiple values at once like this
+console.log(playerName, playerAttack, playerHealth);
+
+var enemyName = "Roborto";
+var enemyHealth = 50;
+var enemyAttack = 12;
+
+var fight = function() {
+    // Alert players that they are starting the round
+    window.alert("Welcome to Robot Gladiators!");
+
+    // Subtract value of playerAttack from value of enemyHealth and use that result to update the enemyHealth variable
+    enemyHealth = enemyHealth - playerAttack;
+    //Log a message to console to show it worked.
+    console.log(
+        playerName + " attacked " + enemyName + ". " + enemyName + " now has " + enemyHealth + " health remaining."
+    );
+    //Subtract value of enemyAttack from playerHealth, using result tp update value of playerHealth var
+        playerHealth = playerHealth - enemyAttack;
+    //Log a message to console to show it worked.
+    console.log(
+        enemyName + " attacked " + playerName + ". " + playerName + " now has " + playerHealth + " health remaining."
+    );
+};
+
+fight();
