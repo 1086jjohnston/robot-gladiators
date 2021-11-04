5ec7d5fa20574a40a0cb9cba3045d9aef269ef28
Jordan Johnston <1086jjohnston@gmail.com>
2021-11-04 14:29:45 -0500

    multiple rounds complete

--- assets/js/game.js
+++ assets/js/game.js
@@ -11,6 +11,22 @@ var enemyHealth = 50;
 var enemyAttack = 12;
 
 var fight = function(enemyName) {
+
+    if (playerHealth > 0) {
+        window.alert("Welcome to Robot Gladiators! Round " + (i + 1));
+    
+        var pickedEnemyName = enemyNames[i];
+
+        enemyHealth = 50;
+
+        fight(pickedEnemyName);
+    }
+
+    else {
+        window.alert("You have lost your robot in battle! Game Over!");
+        break;
+    }
+
     while (playerHealth > 0 && enemyHealth > 0) {
       // ask player if they'd like to fight or run
       var promptFight = window.prompt('Would you like to FIGHT or SKIP this battle? Enter "FIGHT" or "SKIP" to choose.');
@@ -65,9 +81,3 @@ var fight = function(enemyName) {
       }
     }
   };
-
-for(var i = 0; i < enemyNames.length; i++) {
-    var pickedEnemyName = enemyNames[i];
-    enemyHealth = 50;
-    fight(pickedEnemyName[i]);
-}
